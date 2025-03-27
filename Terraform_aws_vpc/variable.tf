6da07dc4fff5a3ba6c08dcb8d485d34607ca328e
Vineeth Naik <weneeth2019@gmail.com>
2025-03-27 18:25:05 -0500

    chnages in variable file

--- Terraform_aws_vpc/variable.tf
+++ Terraform_aws_vpc/variable.tf
@@ -1,32 +1,25 @@
 variable "cidr" {
   default = "10.0.0.0/16"
-}
-
-variable "availability_zones" {
-  description = "values of availability zones"
-  type = list(string)
-  default = ["us-west-2a", "us-west-2b", "us-west-2c"]  
   
 }
 
 variable "aws_ami" {
   description = "AMI ID"
   type = string
-  default = "ami-08b5b3a93ed654d19"
   
 }
 
 variable "instance_type" {
   description = "Instance type"
   type = string
-  default = "t2.micro"
+  
   
 }
 
 variable "key_name" {
   description = "Key pair name"
   type = string
-  default = "EC2 Beginner"
+   
   
 }
 
